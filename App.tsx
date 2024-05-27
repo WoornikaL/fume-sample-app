ebaec0d5b9ddb80f057b87dc391879e185f9060a
FumeDev <FumeDev@users.noreply.github.com>
2024-05-27 04:33:35 +0000

    "Implemented React Navigation setup and integrated it with the main application files (iOS)."

--- App.tsx
+++ App.tsx
@@ -79,3 +79,22 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginView from './src/screens/authentication/LoginView';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginView} />
+        {/* Add more screens as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
